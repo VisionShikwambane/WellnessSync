a35847031c4db5d4c81719bd0621b2923361f6d1
Vision Shikwambane <visionvee201@gmail.com>
2024-03-11 11:13:43 +0200

    Card 1 on homepage done

--- src/app/patient/patient-home-tab/patient-home-tab.page.ts
+++ src/app/patient/patient-home-tab/patient-home-tab.page.ts
@@ -1,18 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import {notificationsOutline } from 'ionicons/icons';
 import { addIcons } from 'ionicons';
+import {Swiper} from 'swiper';
+import { register } from 'swiper/element/bundle';
+
+register();
 
 @Component({
   selector: 'app-patient-home-tab',
   templateUrl: './patient-home-tab.page.html',
   styleUrls: ['./patient-home-tab.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class PatientHomeTabPage implements OnInit {
+
+  
+
+
+  swiper = new Swiper('.swiper', {
+    autoplay: {
+      delay: 5000,
+    },
+   });
 
   constructor() { 
     addIcons({ notificationsOutline})
